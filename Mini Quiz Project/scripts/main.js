47f64883e98f81edcab22a22ad26878344f22796
Sierra Smith <sierrasmith5819@gmail.com>
2025-09-18 02:06:55 -0400

    Local Storage Use, BootStrap
    Started implementing local storage and using time stamps for storing + caching our quiz questions. Much more of this to come. Also began styling with BootStrap.

--- Mini Quiz Project/scripts/main.js
+++ Mini Quiz Project/scripts/main.js
@@ -6,7 +6,9 @@ const quizPage = document.getElementById("quiz-page");
 const difficultyCheckboxes = document.querySelectorAll('input[type="checkbox"][name="difficulty"]');
 let numOfQuestions = document.getElementById("num_of_questions");
 let selectedDifficulty = [];
-
+let selectCategory = document.getElementById("categories");
+const categoryArea = document.getElementById("category-text-area");
+const categoryTextNode = document.createTextNode("");
 const questionArea = document.getElementById("question-area");
 const quizMessageBox = document.getElementById("quiz-message-box");
 const difficultyLevel = document.getElementById("difficulty-level");
@@ -19,7 +21,8 @@ const qNumberTextNode = document.createTextNode("");
 // buttons
 const btnStart = document.getElementById("btn-start");
 const btnOptions = document.getElementById("btn-options");
-const btnsaveOptions = document.getElementById("btn-save-options");
+const btnOkay = document.getElementById("btn-okay");
+const btnSaveOptions = document.getElementById("btn-save-options");
 const btnCancelOptions = document.getElementById("btn-cancel-options");
 const btnCheckAnswer = document.getElementById("btn-check-answer");
 const btnNext = document.getElementById("btn-next"); 
@@ -29,54 +32,244 @@ const allButtons = document.querySelectorAll("button");
 
 // modals
 let modalContainer = document.getElementById("modal-container");
+let modalContainerQuizOptions = document.getElementById("modal-container-quiz-options");
+let undefinedDifficultyModal = document.getElementById("undefined-difficulty-modal");
 let winModal = document.getElementById("win-modal");
 let loseModal = document.getElementById("lose-modal");
 let endModal = document.getElementById("end-modal");
 
 // dynamic variables for quiz logic
+const TWENTY_FOUR_HOURS = 86400000;
+
 let selectedAnswer;
 let pointValue;
 let questionNumber = 1;
 let counter = 0;
 let score = 0;
 
-function buildApiString() {
+// fetching questions for API call
+function buildApiString(questionAmount, questionCategory, questionDifficulty) {
 
-    return "https://opentdb.com/api.php?amount="+questionAmount+"&category=28&difficulty="+questionDifficulty+"&type=multiple";
+    return "https://opentdb.com/api.php?amount="+questionAmount+"&category="+questionCategory+"&difficulty="+questionDifficulty+"&type=multiple";
 
 }
 
-// fetching questions for API call
-// (defauly question difficulty and question amount)
-let questionDifficulty = "medium";
+
+// (default question difficulty and question amount)
 let questionAmount = 5;
-let url = buildApiString();
+let questionCategory = 9; // Category 9 = ANY
+let questionDifficulty = "medium";
+const categoriesUrl = "https://opentdb.com/api_category.php";
+let url = buildApiString(questionAmount, questionCategory, questionDifficulty);
 
 
+let question_bank = [];
+
+let question_meta_info = {
+
+    last_accessed: ""
+
+};
+
+// where all the question objects are stored
+let quizQuestions = [];
+
+function randomizeAnswers(answersToRandomize){
+
+    let newAnswersAndAnswerIndex = {};
+
+    let correctAnswer = answersToRandomize[answersToRandomize.length - 1];
+
+    // console.log("Correct Answer:" + correctAnswer + " ");
+    // console.log(answersToRandomize);
+
+    newAnswersAndAnswerIndex.answers = shuffleArray(answersToRandomize);
+
+    for (let i = 0; i < newAnswersAndAnswerIndex.answers.length; i++) {
+
+        if (newAnswersAndAnswerIndex.answers[i] == correctAnswer) {
+
+            newAnswersAndAnswerIndex.correctAnswerIndex = i;
+            break;
+        }
+    }
+
+    // console.log(newAnswersAndAnswerIndex);
+    return newAnswersAndAnswerIndex;
+}
+
+function shuffleArray(array) {
+
+    let currentIndex = array.length;
+    let randomIndex;
+
+    // while there remain elements to shuffle,
+    while (currentIndex !== 0 ) {
+
+        // pick a remaining element
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // and swap it with the current element
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]
+        ]
+
+    }
+
+  return array;
+}
+
+let questionCategories = [];
+
+async function getCategories(categoriesUrl) {
+
+    try {
+
+        // check to see if the questions are in the bank, get new questions if they are not
+        const cache = JSON.parse(localStorage.getItem("question-categories") || "[]");
+        const meta = JSON.parse(localStorage.getItem("question-meta-info") || "{}")
+
+        const last_fetched = Number(meta.last_fetched) || 0;
+        const ageMs = Date.now() - last_fetched;
+        console.log("Age since last API fetch (ms): ", ageMs);
+
+        // if cache exists and it's "fresh enough"
+        if (cache.length > 0 && ageMs < 2000) { // 2 seconds
+
+            quizQuestions = cache;
+            console.log("loaded cache categories")
+            return;
+        }
+        
+        // fetch the url containing the question categories
+        const res = await fetch(categoriesUrl);
+        const data = await res.json();
+
+        if (!data || !data.trivia_categories) {
+            console.warn("Categories undefined, retrying...");
+            setTimeout(getCategories, 1000);
+            return;
+        }
+
+        console.log("We have categories!");
+
+        for (let element of data.trivia_categories) {
+            let thisCategory = {
+                id: element.id,
+                name: element.name
+            };
+
+        questionCategories.push(thisCategory);
+
+        }
+        localStorage.setItem("question-categories", JSON.stringify(questionCategories));
+        quizQuestions = question_bank;
+
+        meta.last_fetched = Date.now();
+        localStorage.setItem("question-meta-info", JSON.stringify(meta));
+        console.log("API loaded new data: " + JSON.parse(localStorage.getItem("question-meta-info")));
+
+    } catch (err) {
+        console.error("Error fetching Categories:", err);
+        setTimeout(getCategories, 1000);
+
+    } 
+
+    // console.log(questionCategories);
+
+    questionCategories.forEach(category => {
+
+        const option = document.createElement('option');
+        option.value = category.id;
+        option.textContent = category.name;
+        selectCategory.appendChild(option);
+
+    })
+
+    // set the default of questionCategories to questionCategory(9)
+    questionCategories.value = questionCategory;
+
+}
+
+getCategories(categoriesUrl);
+
+// builds question objects based on the API url passed into it
 async function getQuestions(){
 
     try {
 
-        let res = await fetch(url);
-        let data = await res.json();
+        // check to see if the questions are in the bank, get new questions if they are not
+        const cache = JSON.parse(localStorage.getItem("question-bank") || "[]");
+        const meta = JSON.parse(localStorage.getItem("question-meta-info") || "{}")
+
+        const last_fetched = Number(meta.last_fetched) || 0;
+        const ageMs = Date.now() - last_fetched;
+        console.log("Age since last API fetch (ms): ", ageMs);
+
+        // if cache exists and it's "fresh enough"
+        if (cache.length > 0 && ageMs < 2000) { // 2 sec
+
+            quizQuestions = cache;
+            console.log("loaded cache questions")
+            return;
+        }
+        
+        // otherwise call the API
+        const res = await fetch(url);
+        const data = await res.json();
 
         if (!data || !data.results) {
             console.warn("Data undefined, retrying...");
             setTimeout(getQuestions, 1000);
             return;
         }
+
         console.log("We have questions!");
 
         for (let element of data.results) {
             let thisQuestion = {
                 question: element.question,
-                answers: element.incorrect_answers,
+                // ...element.incorrect_answers is shorthand JS for inserting all
+                // incorrect answers into the answers array without a loop
+                answers: [...element.incorrect_answers, element.correct_answer],
                 correctAnswerIndex: element.incorrect_answers.length,
+                category: element.category,
+                difficulty: element.difficulty,
                 pointValue: 1
             };
 
-            quizQuestions.push(thisQuestion);
+            // determine the point value of each question based on
+            // its difficulty
+            if (thisQuestion.difficulty == "easy") {
+
+            thisQuestion.pointValue = 1;
+
+            } else if (thisQuestion.difficulty == "medium") {
+
+            thisQuestion.pointValue = 3;
+
+            } else if (thisQuestion.difficulty == "hard") {
+
+            thisQuestion.pointValue = 5;
+            }
+
+            // use the randomizeAnswers() function to randomize thisQuestion answers
+            let randomizedAnswers = randomizeAnswers(thisQuestion.answers);
+            thisQuestion.answers = randomizedAnswers.answers;
+            thisQuestion.correctAnswerIndex = randomizedAnswers.correctAnswerIndex;
+            // console.log(thisQuestion);
+            // quizQuestions.push(thisQuestion);
+            question_bank.push(thisQuestion);
         }
+
+        localStorage.setItem("question-bank", JSON.stringify(question_bank));
+        quizQuestions = question_bank;
+
+        meta.last_fetched = Date.now();
+        localStorage.setItem("question-meta-info", JSON.stringify(meta));
+        console.log("API loaded new data: " + JSON.parse(localStorage.getItem("question-meta-info")));
+
     } catch (err) {
         console.error("Error fetching Questions:", err);
         setTimeout(getQuestions, 1000);
@@ -84,16 +277,19 @@ async function getQuestions(){
     } 
 }
 
-let quizQuestions = [];
-
-getQuestions("https://opentdb.com/api.php?amount="+questionAmount+"&category=28&difficulty="+questionDifficulty+"&type=multiple");
-
-console.log(quizQuestions);
+// gets questions according to the results of the default API call
+getQuestions(url);
 
 // display the current score on the page by appending it
 // to the scoreArea h3 element
 scoreTextNode.textContent = score;
 scoreArea.appendChild(scoreTextNode);
+
+// Add an event listener for when the selected option changes
+selectCategory.addEventListener('change', function(event) {
+  const selectedValue = event.target.value;
+  console.log('Selected category + id :', selectedValue);
+});
 
 // function that configures all the buttons at once
 allButtons.forEach(button => {
@@ -103,32 +299,125 @@ allButtons.forEach(button => {
     })
 })
 
-// when the end button is clicked, the quiz ends and the landing
-// page is shown
-btnsEnd.forEach(button => {
-
-    button.addEventListener('click', function(event){
-    
-    questionArea.innerHTML = '';
-    counter = 0;
-
-    hidePage(currentModal);
-    endQuiz();
-
-    })
-})
-
 // when the start button is clicked, the quiz begins
 btnStart.addEventListener('click', function(event){
 
     startQuiz();
 });
 
-btnOptions.addEventListener('click', function(event){
+/* btnOptions.addEventListener('click', function(event){
 
     openOptions();
 
+}); 
+
+function openOptions(){
+
+    hidePage(landingPage);
+    showPage(optionsPage);
+
+} */
+
+btnSaveOptions.addEventListener('click', function(event){
+
+    // clears previously defined selected difficulty options
+    // (prevents duplicates)
+    selectedDifficulty = [];
+
+    // clears the default quizQuestions
+    quizQuestions = [];
+
+    // takes value from numOfQuestions combo box
+    questionAmount = numOfQuestions.value;
+
+    questionCategory = selectCategory.value;
+
+    // stores all the checked checkbox values into selectedDifficulty
+    difficultyCheckboxes.forEach(checkbox => {
+
+        if (checkbox.checked) {
+            selectedDifficulty.push(checkbox.value);
+        }
+    });
+
+    if (selectedDifficulty.length == 0) {
+
+        showPage(modalContainerQuizOptions);
+        showPage(undefinedDifficultyModal);
+
+    } else {
+
+        hidePage(optionsPage);
+        showPage(landingPage);
+
+    }
+
+    let questionCounts = {};
+
+    const questionsPerDifficulty = Math.floor(questionAmount / selectedDifficulty.length);
+    const remainder = questionAmount % selectedDifficulty.length;
+
+    if (selectedDifficulty.length > 1) {
+
+        selectedDifficulty.forEach(difficulty => {
+
+            questionDifficulty = difficulty;
+
+            questionCounts[difficulty] = questionsPerDifficulty;
+
+        })
+
+    }
+
+    // AI: Distribute remainder (e.g., if questionAmount = 10, give the extra 1 to one difficulty)
+    for (let i = 0; i < remainder; i++) {
+
+        questionCounts[selectedDifficulty[i]]++;
+    }
+
+        console.log(questionCounts);
+
+    // key, value = selectedDifficulty, questionsPerDifficulty
+    Object.entries(questionCounts).forEach(([key, value]) => {
+
+        url = buildApiString(value, questionCategory,`${key}`);
+        getQuestions(url);
+
+        console.log(url);
+
+    })
+
+    console.log(quizQuestions);
+
 });
+
+btnCancelOptions.addEventListener('click', function(event){
+
+    // reset all input fields to default values
+    numOfQuestions.value = questionAmount;
+    selectCategory.value = questionCategory;
+
+    // Loop through each difficulty checkbox
+    difficultyCheckboxes.forEach(checkbox => {
+        // Example condition: check only if value is 'banana' or 'cherry'
+        if (checkbox.value === 'medium') {
+            checkbox.checked = true; // ✅ Check it
+        } else {
+            checkbox.checked = false; // ❌ Uncheck it
+        }
+    });
+
+    hidePage(optionsPage);
+    showPage(landingPage);
+
+});
+
+btnOkay.addEventListener('click', function(event) {
+
+    hidePage(modalContainerQuizOptions);
+    hidePage(undefinedDifficultyModal);
+
+})
 
 // when the check answer button is pressed, the user's answer
 // is assessed based on the quizAnswers array
@@ -148,38 +437,29 @@ btnTryAgain.addEventListener('click', function(event) {
     hidePage(currentModal);
 })
 
-/*
-let quizQuestions = [
+// when the end button is clicked, the quiz ends and the landing
+// page is shown
+btnsEnd.forEach(button => {
 
-    {"question": "What color is the sky?",
-    "answers": ['red', 'blue', 'yellow', 'turtles'],
-    "correctAnswerIndex": 1,
-    "pointValue": 1},
+    button.addEventListener('click', function(event){
+    
+    questionArea.innerHTML = '';
+    counter = 0;
 
-    {"question": "What is the largest sea in the world?",
-    "answers": ['Arabian Sea', 'Coral Sea', 'Carribean Sea', 'Philippine Sea'],
-    "correctAnswerIndex": 3,
-    "pointValue": 2},
+    hidePage(currentModal);
+    endQuiz();
 
-    {"question": "What galaxy are we in?",
-    "answers": ['Andromeda', 'Milky Way', 'Canis Major Dwarf', 'Snickers'],
-    "correctAnswerIndex": 1,
-    "pointValue": 1},
-
-    {"question": "What is the rarest element in the universe?",
-    "answers": ['Palladium', 'Oxygen', 'Astatine', 'Scandium'],
-    "correctAnswerIndex": 2,
-    "pointValue": 3},
-
-    {"question": "What cannot be found in space?",
-    "answers": ['Sound waves', 'Diamonds', 'Gravity', 'Ice giants'],
-    "correctAnswerIndex": 0,
-    "pointValue": 3},
-
-]; */
+    })
+})
 
 
 function startQuiz(){
+
+    console.log(quizQuestions);
+
+    console.log("question category:" + questionCategory);
+
+    categoryArea.textContent = quizQuestions[counter].category;
 
     qNumberTextNode.textContent = questionNumber;
     questionNumberArea.appendChild(qNumberTextNode);
@@ -190,44 +470,13 @@ function startQuiz(){
     showPage(quizPage);
 }
 
-function openOptions(){
 
-    hidePage(landingPage);
-    showPage(optionsPage);
-
-}
-
-btnsaveOptions.addEventListener('click', function(event){
-
-
-    questionAmount = numOfQuestions.value;
-    
-    difficultyCheckboxes.forEach(checkbox => {
-
-        if (checkbox.checked) {
-            selectedDifficulty.push(checkbox.value);
-        }
-
-    });
-
-    console.log(selectedDifficulty);
-    console.log(selectedNumOfQuestions);
-
-    hidePage(optionsPage);
-    showPage(landingPage);
-
-});
-
-btnCancelOptions.addEventListener('click', function(event){
-
-    hidePage(optionsPage);
-    showPage(landingPage);
-
-});
 
 function endQuiz(){
 
     score = 0;
+    questionNumber = 1;
+    categoryArea.textContent = "";
     hidePage(modalContainer);
     hidePage(currentModal);
     hidePage(quizPage);
@@ -255,41 +504,27 @@ function nextQuestion(){
 function setupQuizQuestion(quizQuestions){
 
     let currentQuestion = quizQuestions[counter].question;
+    let currentDifficulty = quizQuestions[counter].difficulty
     let answerChoices = quizQuestions[counter].answers;
     pointValue = quizQuestions[counter].pointValue;
 
-    console.log(counter);
-    console.log(currentQuestion);
-    console.log(answerChoices);
+    // console.log(counter);
+    // console.log(currentQuestion);
+    // console.log(answerChoices);
 
     //Create the paragraph to ask the question
     let questionText = document.createElement("h2");
     questionText.innerText = currentQuestion;
 
-    console.log(questionText.innerText);
+    // console.log(questionText.innerText);
 
     
     questionArea.appendChild(questionText);
 
-    // append the difficulty level to difficultyLevel depending
-    // on the question's point value
-    if (pointValue == 1) {
-
-        difficulty = "Easy";
-
-    } else if (pointValue == 2) {
-
-        difficulty = "Medium";
-
-    } else if (pointValue >= 3) {
-
-        difficulty = "Hard";
-
-    }
-
+    
     // after the difficulty level is evaluated, append it
     // to the difficultyLevel p element
-    difficultyTextNode.textContent = difficulty;
+    difficultyTextNode.textContent = currentDifficulty;
     difficultyLevel.appendChild(difficultyTextNode);
 
     //Create the unordered list to display the answers
@@ -300,7 +535,7 @@ function setupQuizQuestion(quizQuestions){
         option.value = i;
         option.innerText = answerChoices[i];
 
-        console.log(option.value);
+        // console.log(option.value);
         selectBox.appendChild(option);
     }
 
